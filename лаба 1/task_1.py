aa8631e2f9b745b7202250b67ba27903c7c6c350
S H E R R Y <89315757+KenjIsanl@users.noreply.github.com>
2024-12-12 21:21:29 +0300

    Update task_1.py

--- лаба 1/task_1.py
+++ лаба 1/task_1.py
@@ -1,55 +1,185 @@
 import doctest
 
 
-# TODO: описать класс
+class Fridge:
+    def __init__(self, is_open: bool, capacity_volume, occupied_volume: int):
+        """
+        Создание и подготовка к работе объекта "Холодильник"
+
+        :param capacity_volume: Объем холодильника
+        :param occupied_volume: Объем еды в холодильнике
+
+        Примеры:
+        >>> fridge = Fridge(True, 500, 0)  # инициализация экземпляра класса
+        """
+        if not isinstance(capacity_volume, int):
+            raise TypeError("Объем холодоса должен быть типа int")
+        if capacity_volume <= 0:
+            raise ValueError("Объем холодоса должен быть положительным числом")
+        self.capacity_volume = capacity_volume
+
+        if not isinstance(occupied_volume, int):
+            raise TypeError("Количество еды должно быть int")
+        if occupied_volume < 0:
+            raise ValueError("Количество еды не может быть отрицательным числом")
+        self.occupied_volume = occupied_volume
+
+        self.is_open = is_open
+
+    def is_fridge_open(self) -> bool:
+        """
+        Функция которая проверяет является ли является холодильник открытым
+
+        :return: Является ли открытым холодильник
+
+        Примеры:
+        >>> fridge = Fridge(True, 500, 0)
+        >>> fridge.is_fridge_open()
+        True
+        """
+        return self.is_open
+
+    def add_food_to_fridge(self, food: int) -> None:
+        """
+        Добавляем еду в холодос.
+        :param food: Количество еды, которую добавляем в холодос
+
+        :raise ValueError: Если еда не помощается в холодос, не является целым числом и не является положительным.
 
 
-class Book:
-    def __init__(self, title: str, genre: str):
-        self.title = title
-        self.genre = genre
-
-    book_1 = Book("K", "Romance")
-    book_2 = Book("L", "Romance")
-
-    def _genre_valid(self) -> bool:
-        valid_genres = ["Fantasy", "Mystery", "Romance", "Thriller", "Detective"]
-        return self.genre in valid_genres
-
-    def change_title(self, new_title: str) -> None:
-        self.title = new_title
-
-    print(book_1.title, book_1.genre)
-    print(book_2.title, book_2.genre)
+        Примеры:
+        >>> fridge = Fridge(True, 500, 0)
+        >>> fridge.add_food_to_fridge(155)
+        """
+        if not isinstance(food, int):
+            raise TypeError("Добавляемая еда должна быть типа int")
+        if food < 0:
+            raise ValueError("Добавляемая еда должна положительным числом")
+        if self.capacity_volume > food + self.occupied_volume:
+            self.capacity_volume += food
+        else:
+            raise ValueError("Нет места в холодосе")
 
 
-# TODO: описать ещё класс
-class Games:
-    def __init__(self, title: str, price: int):
-        self.title = title
-        self.price = price
+class Bed:
+    def __init__(self, is_bed_made_up, is_bed_comfortable: bool, number_of_pillows: int):
+        """
+        Создание и подготовка к работе объекта "Кровать"
 
-    def _title_valid(self) -> bool:
-        valid_titles = ["Genshin", "HSR", "Stalker"]
-        return self.title in valid_titles
+        :param number_of_pillows: Количество игрушек
+        :param is_bed_made_up: Заправлена ли кровать
+        :param is_bed_comfortable: Комфортна ли кровать
 
-    def calculate_discounted_price(self, discount: float) -> float:
-        discounted_price = self.price - (self.price * discount / 100)
-        return discounted_price
+        Примеры:
+        >>> bed = Bed(False, True, 1)  # инициализация экземпляра класса
+        """
+        if not isinstance(number_of_pillows, int):
+            raise TypeError("Количество подушек должно быть класса int")
+        if number_of_pillows <= 0:
+            raise ValueError("Количество подушек должно быть положительным числом")
+        self.number_of_pillows = number_of_pillows
+        self.is_bed_made_up = is_bed_made_up
+        self.is_bed_comfortable = is_bed_comfortable
+
+    def is_my_bed_made_up(self) -> bool:
+        """
+        Функция которая проверяет заправлена ли моя кровать
+
+        :return: Является ли моя кровать заправленной
+
+        Примеры:
+        >>> bed = Bed(True, True, 1)
+        >>> bed.is_my_bed_made_up()
+        True
+        """
+        return self.is_bed_made_up
+
+    def add_pillows_on_bed(self, pillows: int) -> None:
+        """
+        Добавляем подушки на кровать.
+        :param pillows: Подушки, которые я добавляю на кровать
 
 
-# TODO: и ещё один
-class Computer:
-    def __init__(self, brand: str, model: str):
-        self.brand = brand
-        self.model = model
+        Примеры:
+        >>> bed = Bed(False, True, 1)
+        >>> bed.add_pillows_on_bed(2)
+        >>> print(bed.number_of_pillows)
+        3
+        """
+        if not isinstance(pillows, int):
+            raise TypeError("Добавляемые подушки должны быть типа int")
+        if pillows < 0:
+            raise ValueError("Добавляемые подушки должны быть положительными")
+        self.number_of_pillows += pillows
 
-    def change_brand(self, new_brand: str) -> None:
-        self.brand = new_brand
+    def is_my_bed_comfortable(self) -> bool:
+        """
+        Функция которая проверяет комфортна ли моя кровать
 
-    def _model_valid(self) -> bool:
-        valid_models = ["ASUS TUF", "HUAWEI"]
-        return self.model in valid_models
+        :return: Является ли моя кровать комфортной
+
+        Примеры:
+        >>> bed = Bed(False, True, 1)
+        >>> bed.is_my_bed_comfortable()
+        False
+        """
+        if self.number_of_pillows >= 3:
+            self.is_bed_comfortable = True
+        else:
+            self.is_bed_comfortable = False
+
+        return self.is_bed_comfortable
+
+
+class Game:
+    def __init__(self, is_open_game: bool, total_score: float, death_counter: int):
+        """
+        Создание и подготовка к работе объекта "Игра"
+
+        :param is_open_game: Открыта ли игра
+        :param death_counter: Количество смертей
+        :param total_score: Конечное количество очков
+
+        Примеры:
+        >>> game = Game(True, 500.5, 3)  # инициализация экземпляра класса
+        """
+        if not isinstance(total_score, (int, float)):
+            raise TypeError("Количество очков должно быть класса int или float")
+        if total_score < 0:
+            raise ValueError("Количество очков должно быть положительным числом")
+        self.is_open_game = is_open_game
+        self.total_score = total_score
+        self.death_counter = death_counter
+
+    def do_i_won(self) -> bool:
+        """
+        Функция которая проверяет выиграл ли я
+
+        :return: Является ли игра выигрышной
+
+        Примеры:
+        >>> game = Game(True, 545.5, 4)
+        >>> game.do_i_won()
+        True
+        """
+        if self.total_score > 400 and self.death_counter < 10:
+            is_won = True
+        else:
+            is_won = False
+        return is_won
+
+    def close_my_game(self) -> None:
+        """
+        Закрытие игры
+
+
+        Примеры:
+        >>> game = Game(True, 500, 3)
+        """
+        if self.is_open_game is True:
+            self.is_open_game = False
+        else:
+            raise ValueError("Игра итак закрыта")
 
 
 if __name__ == "__main__":
